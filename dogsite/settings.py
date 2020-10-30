b03703ea709dd0b9df47fcf8cc9e0f9072f0daa5
groth001 <groth@unomaha.edu>
2020-10-29 21:17:44 -0700

    Created api app

--- dogsite/settings.py
+++ dogsite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
-    'rest_framework',
+    #'api',
+    #'rest_framework',
 ]
 
 MIDDLEWARE = [

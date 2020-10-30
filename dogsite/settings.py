0f5ad1a1d7b0cc1553fd79560c17dfa7f66053c3
groth001 <groth@unomaha.edu>
2020-10-29 21:07:52 -0700

    Revert ALLOWED_HOSTS

--- dogsite/settings.py
+++ dogsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '<insert secret key here>'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [localhost]
+ALLOWED_HOSTS = []
 
 
 # Application definition

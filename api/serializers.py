6c6890a04b0787507bfa9ecd48e03be9dbf18342
groth001 <groth@unomaha.edu>
2020-10-30 20:47:20 -0700

    Fix serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,12 @@
+from rest_framework import serializers
+from api.models import Dog, Breed
+
 class DogSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Snippet
+        model = Dog
         fields = ['name', 'age', 'breed', 'gender', 'color', 'favoritefood', 'favoritetoy']
 
 class BreedSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Snippet
+        model = Breed
         fields = ['name', 'size', 'friendliness', 'trainability', 'sheddingamount', 'exerciseneeds']
